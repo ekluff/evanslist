0f305269d160dda70cbb89a86e7367c60ff06d8f
Evan Clough & Bunker <student@epicodus.com>
2015-09-28 14:24:30 -0700

    refactor code with proper firebase naming conventions

--- app/models/post.js
+++ app/models/post.js
@@ -6,5 +6,4 @@ export default DS.Model.extend({
   body: DS.attr(),
   category: DS.belongsTo('category', {async: true}),
   comments: DS.hasMany('comment', {async: true})
-
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,15 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    debugger;
     return this.store.findAll('category');
-    debugger;
   },
-  actions: {
-    savePost: function(params) {
-      var newPost = this.store.createRecord('post', params);
-      newPost.save();
-      this.transitionTo('index');
-    }
-  }
+  // actions: {
+  //   savePost: function(params) {
+  //     var newPost = this.store.createRecord('post', params);
+  //     newPost.save();
+  //     this.transitionTo('index');
+  //   }
+  // }
 });
