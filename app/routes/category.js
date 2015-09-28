b5028edc08586407ee856f623372c67bac3f2797
Evan Clough & Bunker <student@epicodus.com>
2015-09-28 16:19:27 -0700

    add delete category function

--- app/routes/category.js
+++ app/routes/category.js
@@ -7,9 +7,33 @@ export default Ember.Route.extend({
   actions: {
     savePost: function(params) {
       var newPost = this.store.createRecord('post', params);
-      newPost.save();
-      params.category.save();
+      newPost.save().then(function() {
+        params.category.save().then(function() {
+          newPost.reload();
+        });
+      });
       this.transitionTo('index');
+    },
+    deleteCategory: function(category) {
+      if (confirm('Delete Category')) {
+        // category.get('posts').then(function(posts) {
+        //   posts.forEach(function(post){
+        //     post.destroyRecord();
+        //   });
+        // });
+        category.destroyRecord();
+        this.transitionTo('index');
+      }
+    },
+
+    editCategory: function(category, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key] !== undefined) {
+          category.set(key, params[key]);
+        }
+      });
+      category.save();
+      this.transitionTo('category');
     }
   }
 });
